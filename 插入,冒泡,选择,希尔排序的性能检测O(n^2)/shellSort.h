0d9852034ca057b111598f623821a93db59ae101
Tom-shushu <tom_shushu@sina.com>
2018-11-24 15:59:53 +0800

    ShellSort
    希尔排序

---
+++ 插入,冒泡,选择,希尔排序的性能检测O(n^2)/shellSort.h
@@ -0,0 +1,36 @@
+﻿
+#ifndef OPTIONAL_02_SHELL_SORT_INSERTIONSORT_H
+#define OPTIONAL_02_SHELL_SORT_INSERTIONSORT_H
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+
+template<typename T>
+void shellSort(T arr[], int n){
+    // 计算 increment sequence: 1, 4, 13, 40, 121, 364, 1093...
+    int h = 1;
+    while( h < n/3 )
+        h = 3 * h + 1;
+
+    while( h >= 1 ){
+
+        // h-sort the array
+        for( int i = h ; i < n ; i ++ ){
+
+            // 对 arr[i], arr[i-h], arr[i-2*h], arr[i-3*h]... 使用插入排序
+            T e = arr[i];
+            int j;
+            for( j = i ; j >= h && e < arr[j-h] ; j -= h )
+                arr[j] = arr[j-h];
+            arr[j] = e;
+        }
+
+        h /= 3;
+    }
+}
+
+#endif 
+
